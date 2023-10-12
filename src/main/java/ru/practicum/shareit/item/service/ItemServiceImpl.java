4ca0dd0d542a023b8409251e2e19c74cb5c933ae
Artem Smirnov <kaban1106@yandex.ru>
2023-10-12 05:23:41 +0300

    checkstyle1

--- src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
@@ -173,10 +173,10 @@ public class ItemServiceImpl implements ItemService {
 		BookingDto nextBooking = BookingMapper.toBookingDto(bookingRepository.findFirstByItemAndStartAfterAndStatusOrderByStartAsc(item, time, BookingStatus.APPROVED));
 		cropBookingDto(lastBooking);
 		cropBookingDto(nextBooking);
-		List<CommentDto> comments = CommentMapper.toListCommentDto(commentRepository.findByItem(item));
+//		List<CommentDto> comments = CommentMapper.toListCommentDto(commentRepository.findByItem(item));
 		itemDto.setLastBooking(lastBooking);
 		itemDto.setNextBooking(nextBooking);
-		itemDto.setComments(comments);
+//		itemDto.setComments(comments);
 		return itemDto;
 	}
 
