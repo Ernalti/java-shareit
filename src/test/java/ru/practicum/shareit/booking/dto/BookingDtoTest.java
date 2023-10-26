f13f28761fc81e33d66e668ef20a0b602018607a
Artem Smirnov <kaban1106@yandex.ru>
2023-10-26 12:08:31 +0300

    checkstile

--- src/test/java/ru/practicum/shareit/booking/dto/BookingDtoTest.java
+++ src/test/java/ru/practicum/shareit/booking/dto/BookingDtoTest.java
@@ -9,7 +9,6 @@ import ru.practicum.shareit.booking.enums.BookingStatus;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
