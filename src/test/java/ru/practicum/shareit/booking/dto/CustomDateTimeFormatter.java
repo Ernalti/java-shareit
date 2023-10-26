0d152527fa31b4aeead5764965671e01f92591c2
Artem Smirnov <kaban1106@yandex.ru>
2023-10-26 12:11:39 +0300

    checkstile

--- src/test/java/ru/practicum/shareit/booking/dto/CustomDateTimeFormatter.java
+++ src/test/java/ru/practicum/shareit/booking/dto/CustomDateTimeFormatter.java
@@ -3,6 +3,6 @@ package ru.practicum.shareit.booking.dto;
 import java.time.format.DateTimeFormatter;
 
 public class CustomDateTimeFormatter {
-	public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSSS");
+	public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSS");
 
 }
