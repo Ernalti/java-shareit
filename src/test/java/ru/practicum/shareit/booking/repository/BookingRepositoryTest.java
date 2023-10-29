d04312f5b5eda9a8d56ff80e6453dc85a63866a5
Artem Smirnov <kaban1106@yandex.ru>
2023-10-30 02:50:48 +0300

    tests

--- src/test/java/ru/practicum/shareit/booking/repository/BookingRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/booking/repository/BookingRepositoryTest.java
@@ -89,7 +89,7 @@ class BookingRepositoryTest {
 	public void shouldFindByItemOwnerAndStartBeforeAndEndAfter() {
 		Booking booking = new Booking(0, LocalDateTime.now().minusMinutes(30), LocalDateTime.now().plusMinutes(30), item1, user1, BookingStatus.APPROVED);
 		bookingRepository.save(booking);
-		List<Booking> bookings = bookingRepository.findByItemOwnerAndStartBeforeAndEndAfter(user1, LocalDateTime.now(), LocalDateTime.now(), PageRequest.of(0, 10), Sort.by(Sort.Order.asc("id"))).toList();
+		List<Booking> bookings = bookingRepository.findByItemOwnerAndStartBeforeAndEndAfter(user1, LocalDateTime.now(), LocalDateTime.now(), PageRequest.of(0, 10)).toList();
 		assertEquals(1, bookings.size());
 		Booking newBooking = bookings.get(0);
 		assertTrue(booking.getId() > 0);
@@ -162,7 +162,7 @@ class BookingRepositoryTest {
 		Booking booking = new Booking(0, LocalDateTime.now().minusHours(10), LocalDateTime.now().minusHours(2), item1, user1, BookingStatus.APPROVED);
 		bookingRepository.save(booking);
 
-		List<Booking> bookings = bookingRepository.findByItemOwnerAndEndBeforeAndStatus(user1, LocalDateTime.now(), BookingStatus.APPROVED, PageRequest.of(0, 10), Sort.by(Sort.Order.asc("id"))).toList();
+		List<Booking> bookings = bookingRepository.findByItemOwnerAndEndBeforeAndStatus(user1, LocalDateTime.now(), BookingStatus.APPROVED, PageRequest.of(0, 10)).toList();
 		assertEquals(1, bookings.size());
 		Booking newBooking = bookings.get(0);
 		assertTrue(newBooking.getId() > 0);
@@ -174,7 +174,7 @@ class BookingRepositoryTest {
 	public void shouldFindByItemOwnerAndEndAfter() {
 		Booking booking = new Booking(0, LocalDateTime.now().plusMinutes(10), LocalDateTime.now().plusHours(2), item1, user1, BookingStatus.APPROVED);
 		bookingRepository.save(booking);
-		List<Booking> bookings = bookingRepository.findByItemOwnerAndEndAfter(user1, LocalDateTime.now(), PageRequest.of(0, 10), Sort.by(Sort.Order.asc("id"))).toList();
+		List<Booking> bookings = bookingRepository.findByItemOwnerAndEndAfter(user1, LocalDateTime.now(), PageRequest.of(0, 10)).toList();
 		assertEquals(1, bookings.size());
 		Booking newBooking = bookings.get(0);
 		assertTrue(newBooking.getId() > 0);
@@ -186,7 +186,7 @@ class BookingRepositoryTest {
 	public void shouldFindByItemOwnerAndStatus() {
 		Booking booking = new Booking(0, LocalDateTime.now(), LocalDateTime.now().plusHours(2), item1, user1, BookingStatus.APPROVED);
 		bookingRepository.save(booking);
-		List<Booking> bookings = bookingRepository.findByItemOwnerAndStatus(user1, BookingStatus.APPROVED, PageRequest.of(0, 10), Sort.by(Sort.Order.asc("id"))).toList();
+		List<Booking> bookings = bookingRepository.findByItemOwnerAndStatus(user1, BookingStatus.APPROVED, PageRequest.of(0, 10)).toList();
 		assertEquals(1, bookings.size());
 		Booking newBooking = bookings.get(0);
 		assertTrue(newBooking.getId() > 0);
@@ -199,7 +199,7 @@ class BookingRepositoryTest {
 		Booking booking = new Booking(0, LocalDateTime.now(), LocalDateTime.now().plusHours(2), item1, user1, BookingStatus.APPROVED);
 		bookingRepository.save(booking);
 
-		List<Booking> bookings = bookingRepository.findByBooker(user1, PageRequest.of(0, 10), Sort.by(Sort.Order.asc("id"))).toList();
+		List<Booking> bookings = bookingRepository.findByBooker(user1, PageRequest.of(0, 10)).toList();
 		assertEquals(1, bookings.size());
 		Booking newBooking = bookings.get(0);
 		assertTrue(newBooking.getId() > 0);
@@ -212,7 +212,7 @@ class BookingRepositoryTest {
 		Booking booking = new Booking(0, LocalDateTime.now().minusMinutes(10), LocalDateTime.now().plusHours(2), item1, user1, BookingStatus.APPROVED);
 		bookingRepository.save(booking);
 
-		List<Booking> bookings = bookingRepository.findByBookerAndStartBeforeAndEndAfter(user1, LocalDateTime.now(), LocalDateTime.now(), PageRequest.of(0, 10), Sort.by(Sort.Order.asc("id"))).toList();
+		List<Booking> bookings = bookingRepository.findByBookerAndStartBeforeAndEndAfter(user1, LocalDateTime.now(), LocalDateTime.now(), PageRequest.of(0, 10)).toList();
 		assertEquals(1, bookings.size());
 		Booking newBooking = bookings.get(0);
 		assertTrue(newBooking.getId() > 0);
@@ -225,7 +225,7 @@ class BookingRepositoryTest {
 		Booking booking = new Booking(0, LocalDateTime.MAX.minusMinutes(5), LocalDateTime.now().minusMinutes(1), item1, user1, BookingStatus.APPROVED);
 		bookingRepository.save(booking);
 
-		List<Booking> bookings = bookingRepository.findByBookerAndEndBeforeAndStatus(user1, LocalDateTime.now().plusMinutes(5), BookingStatus.APPROVED, PageRequest.of(0, 10), Sort.by(Sort.Order.asc("id"))).toList();
+		List<Booking> bookings = bookingRepository.findByBookerAndEndBeforeAndStatus(user1, LocalDateTime.now().plusMinutes(5), BookingStatus.APPROVED, PageRequest.of(0, 10)).toList();
 		assertEquals(1, bookings.size());
 		Booking newBooking = bookings.get(0);
 		assertTrue(newBooking.getId() > 0);
@@ -238,7 +238,7 @@ class BookingRepositoryTest {
 		Booking booking1 = new Booking(0, LocalDateTime.now(), LocalDateTime.now().plusMinutes(10), item1, user1, BookingStatus.APPROVED);
 		bookingRepository.save(booking1);
 
-		List<Booking> bookings = bookingRepository.findByBookerAndEndAfter(user1, LocalDateTime.now(), PageRequest.of(0, 10), Sort.by(Sort.Order.asc("id"))).toList();
+		List<Booking> bookings = bookingRepository.findByBookerAndEndAfter(user1, LocalDateTime.now(), PageRequest.of(0, 10)).toList();
 		assertEquals(1, bookings.size());
 		Booking newBooking = bookings.get(0);
 		assertTrue(newBooking.getId() > 0);
@@ -251,7 +251,7 @@ class BookingRepositoryTest {
 		Booking booking = new Booking(0, LocalDateTime.now(), LocalDateTime.now().plusHours(2), item1, user1, BookingStatus.APPROVED);
 		bookingRepository.save(booking);
 
-		List<Booking> bookings = bookingRepository.findByBookerAndStatus(user1, BookingStatus.APPROVED, PageRequest.of(0, 10), Sort.by(Sort.Order.asc("id"))).toList();
+		List<Booking> bookings = bookingRepository.findByBookerAndStatus(user1, BookingStatus.APPROVED, PageRequest.of(0, 10)).toList();
 		assertEquals(1, bookings.size());
 		Booking newBooking = bookings.get(0);
 		assertTrue(newBooking.getId() > 0);
@@ -264,7 +264,7 @@ class BookingRepositoryTest {
 		Booking booking = new Booking(0, LocalDateTime.now(), LocalDateTime.now().plusHours(2), item1, user1, BookingStatus.APPROVED);
 		bookingRepository.save(booking);
 
-		List<Booking> bookings = bookingRepository.findByItemOwner(user1, Sort.by(Sort.Order.asc("id")), PageRequest.of(0, 10)).toList();
+		List<Booking> bookings = bookingRepository.findByItemOwner(user1, PageRequest.of(0, 10,Sort.by(Sort.Order.asc("id")))).toList();
 		assertEquals(1, bookings.size());
 		Booking newBooking = bookings.get(0);
 		assertTrue(newBooking.getId() > 0);
